9db31c5bd6cf70c6b39660e58fdd56f2e03f14f8
Fru1ty <pablo.jofre34@gmail.com>
2020-11-02 03:20:26 +0100

    alla nödvändiga sidor tillaggda
    PogChamp

--- app.js
+++ app.js
@@ -25,4 +25,52 @@ app.get('/about', (req, res) => {
     res.render(clientDir + "about.ejs")
 });
 
+app.get('/generations/1', (req, res) => {
+    res.render(clientDir + "/generations/1.ejs")
+});
+
+app.get('/generations/2', (req, res) => {
+    res.render(clientDir + "/generations/2.ejs")
+});
+
+app.get('/generations/3', (req, res) => {
+    res.render(clientDir + "/generations/3.ejs")
+});
+
+app.get('/generations/4', (req, res) => {
+    res.render(clientDir + "/generations/4.ejs")
+});
+
+app.get('/generations/5', (req, res) => {
+    res.render(clientDir + "/generations/5.ejs")
+});
+
+app.get('/generations/6', (req, res) => {
+    res.render(clientDir + "/generations/6.ejs")
+});
+
+app.get('/generations/7', (req, res) => {
+    res.render(clientDir + "/generations/7.ejs")
+});
+
+app.get('/generations/8', (req, res) => {
+    res.render(clientDir + "/generations/8.ejs")
+});
+
+app.get('/trivia/history', (req, res) => {
+    res.render(clientDir + "/trivia/history.ejs")
+});
+
+app.get('/trivia/about-the-game', (req, res) => {
+    res.render(clientDir + "/trivia/about-the-game.ejs")
+});
+
+app.get('/trivia/ingame-trivia', (req, res) => {
+    res.render(clientDir + "/trivia/ingame-trivia.ejs")
+});
+
+app.get('/trivia/other', (req, res) => {
+    res.render(clientDir + "/trivia/other.ejs")
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
